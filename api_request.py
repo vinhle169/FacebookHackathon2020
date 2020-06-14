c611449f58038c0fa14b6c9a1ec50aeb9b510e7a
Vinh Le <vinhle184@gmail.com>
2020-06-13 19:16:38 -0700

    Successfully requesting the api

--- api_request.py
+++ api_request.py
@@ -1,7 +1,11 @@
 import requests
 
-URL = ""
-r = requests.get(url=URL)
-js = r.json()
-print(js)
+utterance = 'What temperature is it?'
+URL = f'https://api.wit.ai/message?v=20200613&q={utterance}'
+server_Token = '7IK75DZYLY6JPGO45KNY5HLCMAEGIKB4'
+auth_Header = {'Authorization': f'Bearer {server_Token}'}
 
+response = requests.get(url=URL, headers=auth_Header)
+js = response.json()
+
+print(js)
