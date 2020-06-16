d5bc76d6a72f24157ad70740b75a45b2942b9e07
Vinh Le <vinhle184@gmail.com>
2020-06-15 21:36:55 -0700

    Class function for get request from wit.ai api

--- api_request.py
+++ api_request.py
@@ -1,11 +1,23 @@
 import requests
 
-utterance = 'What temperature is it?'
-URL = f'https://api.wit.ai/message?v=20200613&q={utterance}'
-server_Token = '7IK75DZYLY6JPGO45KNY5HLCMAEGIKB4'
-auth_Header = {'Authorization': f'Bearer {server_Token}'}
+class wit:
 
-response = requests.get(url=URL, headers=auth_Header)
-js = response.json()
+    def __init__(self, utter):
+        self.utterance = utter
+        URL = f'https://api.wit.ai/message?v=20200615&q={self.utterance}'
+        server_Token = 'XHAXSXINWBD5KFLNDL5AQWXO4O7672EC'
+        auth_Header = {'Authorization': f'Bearer {server_Token}'}
+        response = requests.get(url=URL, headers=auth_Header).json()
+        self.intent = response['intents'][0]['name']
+        self.entities = {val[0]['name']: (val[0]['value'], val[0]['confidence']) for val in response['entities'].values()}
 
-print(js)
+        if 'traits' in response.keys():
+            self.traits = {key: (val[0]['value'], val[0]['confidence']) for key, val in response['traits'].items()}
+        else:
+            self.traits = None
+
+
+a = wit('Remind me about my appointment tomorrow at 3pm')
+print('Intent: ', a.intent)
+print('Entities: ', a.entities)
+print('Traits: ', a.traits)
