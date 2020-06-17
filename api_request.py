e064bb92a95600ca404fb77f6344f056acc5ee8c
Vinh Le <vinhle184@gmail.com>
2020-06-16 20:35:54 -0700

    updates to api_request, conversation_handler, and intent_handler to deal with remind intent

--- api_request.py
+++ api_request.py
@@ -3,14 +3,18 @@ import requests
 # class to handle the return of a get call to the wit api when given an utterance
 class wit:
 
-    def __init__(self, utter):
+    def __init__(self, utter, new_convo=True):
         self.utterance = utter
         URL = f'https://api.wit.ai/message?v=20200615&q={self.utterance}'
-        server_Token = 'XHAXSXINWBD5KFLNDL5AQWXO4O7672EC'
-        auth_Header = {'Authorization': f'Bearer {server_Token}'}
-        response = requests.get(url=URL, headers=auth_Header).json()
-
-        self.intent = response['intents'][0]['name']
+        self.server_Token = 'XHAXSXINWBD5KFLNDL5AQWXO4O7672EC'
+        self.auth_Header = {'Authorization': f'Bearer {self.server_Token}'}
+        response = requests.get(url=URL, headers=self.auth_Header).json()
+        # print(response)
+        if new_convo:
+            self.intent = response['intents'][0]['name']
+            if len(response['intents']) == 0:
+                print("Unexpected no intent")
+                raise
         self.entities = {val[0]['name']: {'val': val[0]['value'], 'conf': val[0]
                                           ['confidence'], 'role': val[0]['role']} for val in response['entities'].values()}
 
@@ -18,3 +22,4 @@ class wit:
             self.traits = {key: (val[0]['value'], val[0]['confidence']) for key, val in response['traits'].items()}
         else:
             self.traits = None
+
--- conversation_handler.py
+++ conversation_handler.py
@@ -1,22 +1,35 @@
 from api_request import wit
-
+from intent_handler import salutation, express, find, criticism, remind, correct, information
 
 class conversation(wit):
 
     # inherts wit class in order to grab intent, entities, and traits
     def __init__(self, utter):
         super().__init__(utter)
+        self.current_intent = None
+        self.ongoing_intents = {}
 
+    def update_utterance(self, utter):
+        if self.current_intent is None:
+            print('new topic')
+            super().__init__(utter)
+        else:
+            print('continuing convo')
+            super().__init__(utter, new_convo=False)
+        # print('$$conversation updated$$')
 
     # runs the appropriate intent class
     def parse_convo(self):
         intent = getattr(self, self.intent)
-        return intent()
+        self.current_intent = intent
+        return self.current_intent()
 
     # below are the list of methods which run the appropriate classes
     def salutation(self):
+        # add something in the future to save name if name is sent
         s = salutation(self.entities, self.traits)
         s.generate_response()
+        self.current_intent = None
         return s.response
 
     def express(self):
@@ -29,27 +42,31 @@ class conversation(wit):
         pass
 
     def remind(self):
-        pass
+        # add something to save datetime and reminder details
+        if 'remind' not in self.ongoing_intents:
+            self.ongoing_intents['remind'] = remind(self.entities, self.traits)
+            self.ongoing_intents['remind'].generate_response()
+        else:
+            self.ongoing_intents['remind'].generate_response(new_ent=self.entities, new_trait=self.traits)
+            self.current_intent = None
+        return self.ongoing_intents['remind'].response
 
     def correct(self):
         pass
 
-class main_intent:
-
-    def __init__(self, entities, traits):
-        self.entities = entities
-        self.traits = traits
-
-class salutation(main_intent):
-
-    def __init__(self, entities, traits):
-        super().__init__(entities, traits)
-
-    def generate_response(self):
-        self.response = 'Wassup'
-        if 'name' in self.entities:
-            self.response += f" {self.entities['name']['val']}"
+    def information(self):
+        pass
 
 
-x = conversation('Hello my name is Joshua Fajardo')
-print(x.parse_convo())
+if __name__ == '__main__':
+    print('Person: Hey my name is Josh Stupidson')
+    x = conversation('Hey my name is Josh Stupidson')
+    print('Bot: ', x.parse_convo())
+
+    x.update_utterance('Remind me to take my medicine in three minutes')
+    print('Person: Remind me to take my medicine in three minutes')
+    print('Bot: ', x.parse_convo())
+
+    x.update_utterance('In 30 minutes')
+    print('Person: In 30 minutes')
+    print('Bot: ', x.parse_convo())
