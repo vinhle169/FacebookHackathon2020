f83e1b24e0030644580b076b0ba3dedd0c7d20df
Joshua Fajardo <jdfajardo@berkeley.edu>
2020-06-19 21:50:14 -0700

    User can send messages

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ChatFeed, Message } from 'react-chat-ui';
 import styled from 'styled-components';
+import InputGroup from 'react-bootstrap/InputGroup'
 import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
 const Style = styled.div`
   .inputBox {
     display: flex;
     flex-direction: row;
+  }
+
+  .sendButton {
+
   }
 `;
 
@@ -17,11 +23,23 @@ const test_messages = [new Message({id: 1, message: "Hello World!", senderName: 
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {messages: null};
+    this.state = {messages: test_messages, nextMessage: ""};
+    this.sendHandler = this.sendHandler.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler(event) {
+    console.log(event.target.value)
+    this.setState({nextMessage: event.target.value});
+  }
+
+  sendHandler(event) {
+    event.preventDefault();
+    let newMessage = new Message({id: 0, message: this.state.nextMessage});
+    this.setState({messages: this.state.messages.concat(newMessage)});
   }
 
   render() {
-    this.state.messages = test_messages
     return (
       <Style>
         <ChatFeed
@@ -39,14 +57,17 @@ class Home extends React.Component {
             }
             showSenderName
         />
-        <div className="inputBox">
-          <Form>
-            <Form.Control type="text" placeholder="Normal text" />
-          </Form>
-          <Button onClick={} type="submit" className="my-1">
-            Send
-          </Button>
-        </div>
+        <Form onSubmit={this.sendHandler}>
+          <InputGroup>
+            <FormControl
+              placeholder="Send a message..."
+              onChange={this.changeHandler}
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary" type="submit" className="sendButton">Send</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
       </Style>
     )
   }
