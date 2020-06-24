8a41cd4581b4a5d2731ffa37e1eb39631fd4b6ea
Joshua Fajardo <jdfajardo@berkeley.edu>
2020-06-23 18:46:27 -0700

    Using cookies instead of SID

--- back-end/app.py
+++ back-end/app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, emit
 from conversation_handler import conversation
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins=["https://heal-bot.netlify.app"])
+socketio = SocketIO(app, cors_allowed_origins="*")
 user_ids = {}
 
 
@@ -16,23 +16,23 @@ def home():
 def handle_connect():
     global user_ids
     print('NICE')
-    print(request.sid)
-    user_ids.setdefault(request.sid, conversation(''))
+    print(request.cookies.get('username'))
+    user_ids.setdefault(request.cookies.get('username'), conversation(''))
 
 
 @socketio.on('sendMessage')
 def handle_message(message):
     global user_ids
     print(message)
-    print(request.sid + ': ' + message['message'])
-    user_ids[request.sid].update_utterance(message['message'])
-    emit('response', {'message': user_ids[request.sid].parse_convo()})
+    print(request.cookies.get('username') + ': ' + message['message'])
+    user_ids[request.cookies.get('username')].update_utterance(message['message'])
+    emit('response', {'message': user_ids[request.cookies.get('username')].parse_convo()})
 
 
 @socketio.on('disconnect')
 def handle_disconnect():
     global user_ids
-    del user_ids[request.sid]
+    del user_ids[request.cookies.get('username')]
     print('user left')
 
 
